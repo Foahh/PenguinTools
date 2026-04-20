8c64094cbf6d4e978ff39e6c43be2d70d30a3984
Foahh <160589889+Foahh@users.noreply.github.com>
2026-04-20 16:32:35 +0800

    feat: enhance build script to support compression of publish outputs and update publish profiles

--- build.ps1
+++ build.ps1
@@ -2,14 +2,30 @@ $ErrorActionPreference = 'Stop'
 
 $publishTargets = @(
     # Desktop
-    @{ Project = 'PenguinTools/PenguinTools.csproj'; Profile = 'WinX64-SelfContained-SingleFile-EmbeddedAssets' },
-    @{ Project = 'PenguinTools/PenguinTools.csproj'; Profile = 'WinX64-FrameworkDependent-SingleFile-EmbeddedAssets' },
+    @{ Project  = 'PenguinTools/PenguinTools.csproj'; 
+        Profile = 'WinX64-SelfContained-SingleFile-EmbeddedAssets' 
+    },
+    @{ Project  = 'PenguinTools/PenguinTools.csproj';
+        Profile = 'WinX64-FrameworkDependent-SingleFile-EmbeddedAssets' 
+    },
 
     # CLI
-    @{ Project = 'PenguinTools.CLI/PenguinTools.CLI.csproj'; Profile = 'WinX64-SelfContained-SingleFile-EmbeddedAssets' },
-    @{ Project = 'PenguinTools.CLI/PenguinTools.CLI.csproj'; Profile = 'WinX64-SelfContained-SingleFile-ExternalAssets' },
-    @{ Project = 'PenguinTools.CLI/PenguinTools.CLI.csproj'; Profile = 'WinX64-FrameworkDependent-SingleFile-EmbeddedAssets' },
-    @{ Project = 'PenguinTools.CLI/PenguinTools.CLI.csproj'; Profile = 'WinX64-FrameworkDependent-SingleFile-ExternalAssets' }
+    @{ Project  = 'PenguinTools.CLI/PenguinTools.CLI.csproj';
+        Profile = 'WinX64-SelfContained-SingleFile-EmbeddedAssets' 
+    },
+    @{ Project    = 'PenguinTools.CLI/PenguinTools.CLI.csproj'
+        Profile   = 'WinX64-SelfContained-SingleFile-ExternalAssets';
+        ZipSource = 'PenguinTools.CLI/bin/Release/net10.0/publish/WinX64-SelfContained-SingleFile-ExternalAssets';
+        ZipDest   = 'PenguinTools.CLI/bin/Release/net10.0/publish/PenguinTools.CLI-win-x64.zip' 
+    },
+    @{ Project  = 'PenguinTools.CLI/PenguinTools.CLI.csproj';
+        Profile = 'WinX64-FrameworkDependent-SingleFile-EmbeddedAssets' 
+    },
+    @{ Project    = 'PenguinTools.CLI/PenguinTools.CLI.csproj';
+        Profile   = 'WinX64-FrameworkDependent-SingleFile-ExternalAssets';
+        ZipSource = 'PenguinTools.CLI/bin/Release/net10.0/publish/WinX64-FrameworkDependent-SingleFile-ExternalAssets'
+        ZipDest   = 'PenguinTools.CLI/bin/Release/net10.0/publish/PenguinTools.CLI-win-x64-fd-singlefile-external.zip' 
+    }
 )
 
 foreach ($target in $publishTargets) {
@@ -18,6 +34,12 @@ foreach ($target in $publishTargets) {
 
     if ($LASTEXITCODE -ne 0) {
         throw "Publish failed for '$($target.Profile)'."
+    }
+
+    if ($target.ZipDest) {
+        Write-Host "Compressing $($target.Profile)..."
+        if (Test-Path $target.ZipDest) { Remove-Item $target.ZipDest }
+        Compress-Archive -Path "$($target.ZipSource)\*" -DestinationPath $target.ZipDest
     }
 }
 
